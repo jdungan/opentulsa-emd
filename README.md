51e22a311431701b86b1e52ebdbb502e5471792b
jdungan <johnadungan@gmail.com>
2015-04-14 07:21:10 -0500

    add nested job

---
+++ README.md
@@ -0,0 +1,24 @@
+This project is a viewer of data on the OpenTulsa data portal located at https://www.cityoftulsa.org/our-city/open-tulsa/open-tulsa-dataset-list.aspx
+
+The intial dataset is the list of active work orders.
+
+The technologies used for this project are:
+
+- React.js
+- Browserify
+- Gulp 
+
+To install:
+
+'npm install'
+
+To build:
+'gulp build'
+
+To view:
+'python -m SimpleHTTPServer'
+
+Point your browser to http://127.0.0.1:8000/
+
+
+
--- src/js/app.jsx
+++ src/js/app.jsx
@@ -1,10 +1,14 @@
 /** @jsx React.DOM */
 var React = require('react/dist/react');
 var _ = require('underscore');
+var prettydate = require('pretty-date')
 
 var WorkOrderDiv = React.createClass({
   getInitialState: function() {
-      return {data: []};
+    return {data: [],searchString:''}
+    },
+  textSearch: function (e) {
+    this.setState({searchString:e.target.value.toLowerCase()});
     },
   componentDidMount: function() {
     $.ajax({
@@ -22,34 +26,30 @@ var WorkOrderDiv = React.createClass({
   render: function() {
     return (
       <div>
-        <Filter></Filter>
-        <WorkOrderItems data={this.state.data}/>
+        <input placeholder =  "Type here to search" 
+               value = {this.state.searchString} 
+               onChange={this.textSearch} />
+        
+       <WorkOrderList data={this.state.data} search={this.state.searchString}/>
       
       </div>
     );
   }
 });
 
-var Filter = React.createClass({
-  render: function () {
-    
-    return(
-      <form>
-        
-        <input></input>
-      </form>
-    )
-  }
-})
-
-
-var WorkOrderItems = React.createClass({
+var WorkOrderList = React.createClass({
   render: function() {
-    var workorders = this.props.data.map(function (order) {
-      return (
-          <Order data={order}></Order>
-      );
-    });
+    var search =this.props.search
+    workorders = this.props.data
+      .filter(function(order){
+        return order.NotifyName.toLowerCase().match( search );
+      })
+      .map(function (order) {
+        return (
+            <WorkOrder data={order} key ={order.WorkOrderNumber}/>
+        );
+      });
+
     return (
       <ul>
         {workorders}
@@ -58,18 +58,68 @@ var WorkOrderItems = React.createClass({
   }
 });
 
-var Order = React.createClass({
+var WorkOrder = React.createClass({
   render: function() {
+    var diff = new Date() - new Date(this.props.data.OpenDate);    
+    var age = prettydate.format(new Date (new Date() - diff))
     return (
       <li >
-        {this.props.data.WorkOrderNumber}
-        {this.props.data.OpenDate}
-
+        <p>NotifyName: {this.props.data.NotifyName}</p>
+        <p>WorkOrderNumber: {this.props.data.WorkOrderNumber}</p>
+        <p>Status: {this.props.data.Status}</p>
+        <p>UnitNumber: {this.props.data.UnitNumber}</p>
+        <p>CreatedBy: {this.props.data.CreatedBy}</p>
+        <p>CompletedDate: {this.props.data.CompletedDate}</p>
+        <p>CloseDate: {this.props.data.CloseDate}</p>
+        <p>UsingDeptName: {this.props.data.UsingDeptName}</p>
+        <p>LocationName: {this.props.data.LocationName}</p>
+        <p>OpenDate: {this.props.data.OpenDate}</p>
+        <p>Age: { age }</p>
+        <JobList jobs={this.props.data.Jobs.Job}/>
       </li>
     );
   }
 });
 
+
+var JobList = React.createClass({
+  render: function () {
+    var jobs;
+    if (_.isArray(this.props.jobs)) {
+      jobs = _.map(this.props.jobs,function (job) {
+          return(<Job data={job} key = {job.JobNumber }/>)
+      });
+    } else {      
+      jobs = <Job data={this.props.jobs} key = {this.props.jobs.JobNumber }/>
+    }
+      
+    return ( 
+      <ul>
+        {jobs}
+      </ul>
+    )
+  }  
+});
+
+var Job = React.createClass({
+  render: function () {
+    return(
+      <li>
+        <p>JobID: {this.props.data.JobID}</p>
+        <p>JobWorkOrderNumber: {this.props.data.JobWorkOrderNumber}</p>
+        <p>JobReasonCode: {this.props.data.JobReasonCode}</p>
+        <p>JobNumber: {this.props.data.JobNumber}</p>
+        <p>JobDescription: {this.props.data.JobDescription}</p>
+        <p>JobStatus: {this.props.data.JobStatus}</p>
+        <p>JobStatusDescription: {this.props.data.JobStatusDescription}</p>
+        <p>JobStatusPriority: {this.props.data.JobStatusPriority}</p>
+        <p>JobStatusColor: {this.props.data.JobStatusColor}</p>
+        <p>JobCompletedDate: {this.props.data.JobCompletedDate}</p>
+        <p>ActualJobHours: {this.props.data.ActualJobHours}</p>
+      </li>
+    )
+  }
+})
 
 // "https://www.cityoftulsa.org/cot/opendata/OpenData_EMDlist.jsn"
 React.render(
